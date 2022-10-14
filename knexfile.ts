3da017574ff9a5c83861f13bc656e60b9648f1ad
Bruno Sampaio <brunoctsb@gmail.com>
2022-10-14 16:10:28 -0300

    migrations genatarion

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,5 @@
 // Update with your config settings.
 
-import path from "path";
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -9,13 +8,13 @@ import path from "path";
 export const development = {
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, "src", "database", "database.db")
+    filename: './src/database/database.db'
   },
   pool: {
     afterCreate: (conn: any, cb: any) => conn.run("PRAGMA foreign_keys = ON", cb)
   },
   migrations: {
-    directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    directory: './src/database/knex/migrations'
   },
   useNullAsDefault: true,
 };
