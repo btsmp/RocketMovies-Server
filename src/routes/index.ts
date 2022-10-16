6bd363e5021140c744a44558e17de595cb5d9697
Bruno Sampaio <brunoctsb@gmail.com>
2022-10-16 18:38:45 -0300

    bugfix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import express, { Response, Request, NextFunction } from 'express'
+import express from 'express'
 
 import { sessionsRoutes } from "./sessions.routes"
 import { usersRoutes } from "./users.routes"
 import { notesRoutes } from "./notes.routes"
 import { sessionsGoogleRoutes } from "./sessionsGoogle.routes"
+import { UPLOADS_FOLDER } from '../config/upload'
 
 export const routes = Router()
 
